2bbdd503916f1682e2370a3892adc02478c8b4ea
rubaanxd <teckever6@hotmail.com>
2019-11-14 18:05:42 -0600

    Se modifico la clase main para correr la carrera

--- Main/Main.java
+++ Main/Main.java
@@ -1,11 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Main;
 
-import Reloj.FrmContador;
+import Carrera.FrmCarrera;
+
+
 
 /**
  *
@@ -17,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        FrmContador.main(args);
+        FrmCarrera.main(args);
     }
     
 }
